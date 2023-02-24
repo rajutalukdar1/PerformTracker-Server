311c65a971b0e572bec7c17e43f8e0b1e76151d8
Ishtiaq <robin.pass36@gmail.com>
2023-02-24 20:42:48 +0600

    change my code

---
+++ Personal_Information.js
@@ -0,0 +1,224 @@
+
+[
+    {
+        "email": "robin.pass36@gmail.com",
+        "uid": "ATrEsIG8NIND3m8T4TNpXVNOMt93",
+        "personal_info": {
+            "passport_no": "9876543210",
+            "tel": "+880 1762-627422",
+            "nationality": "Bangladeshi",
+            "religion": "Islam",
+            "marital_status": "Unmarried",
+            "employment_of_spouse": "No",
+            "no_of_children": "2"
+        },
+
+        "emergency_contact": {
+            "primary": "John Doe",
+            "relationship": "Father",
+            "secondary": "Karen Wills",
+            "relationship": "Brother",
+            "phone": "+880 1762-627422"
+        },
+
+        "bank_information": {
+            "bank_name": "John ICC Bank",
+            "bank_account_no": "159843014641",
+            "secondary": "Karen Wills",
+            "relationship": "Brother"
+        },
+
+        "education_information": {
+            "ug": "Bsc Computer Science",
+            "ug_year": "2000 - 2003",
+            "pg": "Msc Computer Science",
+            "pg_year": "2000 - 2003"
+        },
+
+        "experience": {
+            "experience_1": "Web Designer at Zen Corporation",
+            "experience_1_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_2": "Web Designer at Ron-tech",
+            "experience_2_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_3": "Web Designer at Dale Technology",
+            "experience_3_join": "Jan 2013 - Present (5 years 2 months)"
+        }
+    },
+    {
+        "email": "raju.pass36@gmail.com",
+        "uid": "ATrEsIG8NIND3m8T4TNpXOMt93",
+        "personal_info": {
+            "passport_no": "9876543210",
+            "tel": "+880 1762-627422",
+            "nationality": "Bangladeshi",
+            "religion": "Islam",
+            "marital_status": "Unmarried",
+            "employment_of_spouse": "No",
+            "no_of_children": "2"
+        },
+
+        "emergency_contact": {
+            "primary": "John Doe",
+            "relationship": "Father",
+            "secondary": "Karen Wills",
+            "relationship": "Brother",
+            "phone": "+880 1762-627422"
+        },
+
+        "bank_information": {
+            "bank_name": "John ICC Bank",
+            "bank_account_no": "159843014641",
+            "secondary": "Karen Wills",
+            "relationship": "Brother"
+        },
+
+        "education_information": {
+            "ug": "Bsc Computer Science",
+            "ug_year": "2000 - 2003",
+            "pg": "Msc Computer Science",
+            "pg_year": "2000 - 2003"
+        },
+
+        "experience": {
+            "experience_1": "Web Designer at Zen Corporation",
+            "experience_1_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_2": "Web Designer at Ron-tech",
+            "experience_2_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_3": "Web Designer at Dale Technology",
+            "experience_3_join": "Jan 2013 - Present (5 years 2 months)"
+        }
+    },
+    {
+        "email": "rofiqul.pass36@gmail.com",
+        "uid": "ATrEsIG8NINDNpXVNOMt93",
+        "personal_info": {
+            "passport_no": "9876543210",
+            "tel": "+880 1762-627422",
+            "nationality": "Bangladeshi",
+            "religion": "Islam",
+            "marital_status": "Unmarried",
+            "employment_of_spouse": "No",
+            "no_of_children": "2"
+        },
+
+        "emergency_contact": {
+            "primary": "John Doe",
+            "relationship": "Father",
+            "secondary": "Karen Wills",
+            "relationship": "Brother",
+            "phone": "+880 1762-627422"
+        },
+
+        "bank_information": {
+            "bank_name": "John ICC Bank",
+            "bank_account_no": "159843014641",
+            "secondary": "Karen Wills",
+            "relationship": "Brother"
+        },
+
+        "education_information": {
+            "ug": "Bsc Computer Science",
+            "ug_year": "2000 - 2003",
+            "pg": "Msc Computer Science",
+            "pg_year": "2000 - 2003"
+        },
+
+        "experience": {
+            "experience_1": "Web Designer at Zen Corporation",
+            "experience_1_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_2": "Web Designer at Ron-tech",
+            "experience_2_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_3": "Web Designer at Dale Technology",
+            "experience_3_join": "Jan 2013 - Present (5 years 2 months)"
+        }
+    },
+    {
+        "email": "sumon.pass36@gmail.com",
+        "uid": "ATrEsNIND3m8T4TNpXVNOMt93",
+        "personal_info": {
+            "passport_no": "9876543210",
+            "tel": "+880 1762-627422",
+            "nationality": "Bangladeshi",
+            "religion": "Islam",
+            "marital_status": "Unmarried",
+            "employment_of_spouse": "No",
+            "no_of_children": "2"
+        },
+
+        "emergency_contact": {
+            "primary": "John Doe",
+            "relationship": "Father",
+            "secondary": "Karen Wills",
+            "relationship": "Brother",
+            "phone": "+880 1762-627422"
+        },
+
+        "bank_information": {
+            "bank_name": "John ICC Bank",
+            "bank_account_no": "159843014641",
+            "secondary": "Karen Wills",
+            "relationship": "Brother"
+        },
+
+        "education_information": {
+            "ug": "Bsc Computer Science",
+            "ug_year": "2000 - 2003",
+            "pg": "Msc Computer Science",
+            "pg_year": "2000 - 2003"
+        },
+
+        "experience": {
+            "experience_1": "Web Designer at Zen Corporation",
+            "experience_1_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_2": "Web Designer at Ron-tech",
+            "experience_2_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_3": "Web Designer at Dale Technology",
+            "experience_3_join": "Jan 2013 - Present (5 years 2 months)"
+        }
+    },
+    {
+        "email": ".pass36@gmail.com",
+        "uid": "ATrEsIG8NIND3m8T4TNpXVNOM",
+        "personal_info": {
+            "passport_no": "9876543210",
+            "tel": "+880 1762-627422",
+            "nationality": "Bangladeshi",
+            "religion": "Islam",
+            "marital_status": "Unmarried",
+            "employment_of_spouse": "No",
+            "no_of_children": "2"
+        },
+
+        "emergency_contact": {
+            "primary": "John Doe",
+            "relationship": "Father",
+            "secondary": "Karen Wills",
+            "relationship": "Brother",
+            "phone": "+880 1762-627422"
+        },
+
+        "bank_information": {
+            "bank_name": "John ICC Bank",
+            "bank_account_no": "159843014641",
+            "secondary": "Karen Wills",
+            "relationship": "Brother"
+        },
+
+        "education_information": {
+            "ug": "Bsc Computer Science",
+            "ug_year": "2000 - 2003",
+            "pg": "Msc Computer Science",
+            "pg_year": "2000 - 2003"
+        },
+
+        "experience": {
+            "experience_1": "Web Designer at Zen Corporation",
+            "experience_1_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_2": "Web Designer at Ron-tech",
+            "experience_2_join": "Jan 2013 - Present (5 years 2 months)",
+            "experience_3": "Web Designer at Dale Technology",
+            "experience_3_join": "Jan 2013 - Present (5 years 2 months)"
+        }
+    }
+
+]
